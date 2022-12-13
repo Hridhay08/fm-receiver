03f34f53fc060b36689e6ce899cbba1b4233f999
Hridhay Natanasubramanian <83655944+Hridhay-NITT@users.noreply.github.com>
2022-12-13 18:31:59 +0530

    physical demonstration media and minor comment removal in a code file

--- rda_freq_tune_atmega/rda_freq_tune_atmega.ino
+++ rda_freq_tune_atmega/rda_freq_tune_atmega.ino
@@ -1,16 +1,3 @@
-/*
-   Test and validation of RDA5807 on ESP32 board.
-    
-   ATTENTION:  
-   Please, avoid using the computer connected to the mains during testing. Used just the battery of your computer. 
-   This sketch was tested on ATmega328 based board. If you are not using a ATmega328, please check the pins of your board. 
-  | RDA5807    | Function              |ESP LOLIN32 WEMOS (GPIO) |
-  |-----------| ----------------------|-------------------------|
-  | SDA/SDIO  |   SDIO                |   21 (SDA / GPIO21)     |
-  | CLK/CLOCK |   SCLK                |   22 (SCL / GPIO22)     |
-   By Ricardo Lima Caratti, 2020.
-*/
-
 #include <RDA5807.h>
 #define MAX_DELAY_RDS 40   // 40ms - polling method
 
